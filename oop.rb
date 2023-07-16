a4ca369fa0dcf324feae147d621b5e609fb2c4db
Neil Hendren <neil.hendren@gmail.com>
2023-07-16 14:40:34 -0600

    Initial commit

--- oop.rb
+++ oop.rb
@@ -3,7 +3,16 @@
 # it should have a color attribute, that is silver by default
 # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
 
+class Unicorn
+    def initialization(name, color = "silver")
+        @name = name;
+        @color = color;
+    end
 
+    def say(words)
+        "*~*#{words}*~*"
+    end
+end
 
 #  Write a class called Vampire
 #  it should have a dynamic name attribute
@@ -11,6 +20,16 @@
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
 
+class Vampire
+    def initialize(pet = "bat", thirsty = true)
+        @pet = pet;
+        @thirsty = thirsty;
+    end
+
+    def drink
+        @thirsty = false;
+    end
+end
 
 #  Write a Dragon class
 #  it should have a dynamic name attribute (string)
@@ -18,6 +37,23 @@
 #  it should have a dynamic color attribute (string)
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
+
+class Dragon
+    def initialize(name, rider, color)
+        @name = name;
+        @rider = rider;
+        @color = color;
+        @is_hungry = true;
+        @times_eaten = 0;
+    end
+
+    def eat
+        @times_eaten += 1;
+        if @times_eaten >= 4
+            @is_hungry = false;
+        end
+    end
+end
 
 
 #  Write a Hobbit class
@@ -28,3 +64,28 @@
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+
+class Hobbit
+    def initialize(name, disposition)
+        @name = name;
+        @disposition = disposition;
+        @age = 0;
+        @is_adult = false;
+        @is_old = false;
+        if name == "Frodo"
+            @has_ring = true;
+        else
+            @has_ring = false;
+        end
+    end
+
+    def celebrate_birthday
+        @age += 1;
+        if @age >= 33
+            @is_adult = true;
+        end
+        if @age >= 101
+            @is_old = true;
+        end
+    end
+end
